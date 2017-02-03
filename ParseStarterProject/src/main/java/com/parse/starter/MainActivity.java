aeafcf4c61bce168510019a4545abdc4f4ea0a5a
garfield_chou <garfield_chou@asus.com>
2017-02-03 10:51:25 +0800

    Create a ParseObject and save it in Parse Server
    1. Create a ParseObject and give it a 'class name', such as, "Score", "User", "ExampleObject"
    2. Assign (put) a value to a variable (i.e., key), such as, "username"-"rob", "score"-86
    3. Save this object in background with a callback. This callback print the ParseException if saved process failed.

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -10,7 +10,12 @@ package com.parse.starter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +24,25 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    ParseObject score = new ParseObject("Score"); // provide a class name
+    score.put("username", "rob"); // key as variable, value as value
+    score.put("score", 86);
+    score.saveInBackground(new SaveCallback() {
+        @Override
+        public void done(ParseException e) {
+
+            if (e == null) {
+
+                Log.i("SaveInBackground", "Successful");
+
+            } else {
+
+                Log.i("SaveInBackground", "Failed. Error: " + e.toString());
+
+            }
+        }
+    });
 
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
