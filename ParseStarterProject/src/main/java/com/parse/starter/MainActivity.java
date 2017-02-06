4cbca92f78cb5e4c0de560849f6b30cb2ee2e0c5
garfield_chou <garfield_chou@asus.com>
2017-02-06 13:32:57 +0800

    Implement the Signup button
    Implement the Signup button
    1, Create a onClick method for the signup button
    2, Get the values of username & password in EditTexts
    3, Do some empty check for the username and password. Show a toast if either one of them is empty.
    4, Signup this user with input username & password
    5, Show error message if signup fails

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -11,6 +11,9 @@ package com.parse.starter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
@@ -27,64 +30,54 @@ import com.parse.SignUpCallback;
 import java.util.List;
 
 
+
 public class MainActivity extends AppCompatActivity {
+
+  public void signUp (View view) {
+
+    EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+
+    EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+    if (usernameEditText.getText().toString().equals("") || passwordEditText.getText().toString().equals("")) {
+
+      Toast.makeText(this, "A username or password are required", Toast.LENGTH_SHORT).show();
+
+    } else {
+
+      ParseUser user = new ParseUser();
+
+      user.setUsername(usernameEditText.getText().toString());
+
+      user.setPassword(passwordEditText.getText().toString());
+
+      user.signUpInBackground(new SignUpCallback() {
+        @Override
+        public void done(ParseException e) {
+
+          if (e == null) {
+
+            Log.i("Signup", "Successful");
+
+          } else {
+
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+          }
+        }
+      });
+
+    }
+
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+
     ParseUser.logOut();
-
-    if (ParseUser.getCurrentUser() != null) {
-
-        Log.i("currentUser", "User logged in " + ParseUser.getCurrentUser().getUsername());
-
-    } else {
-
-        Log.i("currentUser", "User not logged in");
-
-    }
-
-/*
-    ParseUser.logInInBackground("robpercival", "mypass", new LogInCallback() {
-        @Override
-        public void done(ParseUser user, ParseException e) {
-
-            if (user != null) {
-
-                Log.i("Login", "Successful");
-
-            } else {
-
-                Log.i("Login", "Failed: " + e.toString());
-
-            }
-        }
-    });
-
-    ParseUser user = new ParseUser();
-
-    user.setUsername("robpercival");
-    user.setPassword("mypass");
-
-    user.signUpInBackground(new SignUpCallback() {
-        @Override
-        public void done(ParseException e) {
-
-            if (e == null) {
-
-                Log.i("Sign Up", "Successful");
-
-            } else {
-
-                Log.i("Sign Up", "Failed");
-
-            }
-        }
-    });
-*/
-
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
 
