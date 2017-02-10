c808264483fb70050b33b86047d3b1a769aed054
shaudai <shaudai0402@hotmail.com>
2017-02-10 13:14:54 +0800

    Use a LinearLayout to display several images
    1, Add a new Activity and put a LinearLayout in it
    2, Setup a ImageView
    3, Add the ImageView to the LinearLayout

---
+++ ParseStarterProject/src/main/java/com/parse/starter/UserFeedActivity.java
@@ -0,0 +1,30 @@
+package com.parse.starter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class UserFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_feed);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+
+        ImageView imageView = new ImageView(getApplicationContext());
+
+        imageView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+
+        imageView.setImageDrawable(getResources().getDrawable(R.drawable.instagramlogo));
+
+        linearLayout.addView(imageView);
+
+    }
+}
